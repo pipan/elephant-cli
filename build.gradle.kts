8ed83ce93ca5bc03db65a20187d9f96dbc130d5b
Peter Gasparik <gasparik.pe@gmail.com>
2022-03-19 23:42:11 +0100

    v0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,6 @@ application {
 }
 
 group = "com.pipan.elephant"
-version = "0.2.1"
+version = "0.3.0"
 description = "CLI for deploying PHP applications"
 java.sourceCompatibility = JavaVersion.VERSION_11
